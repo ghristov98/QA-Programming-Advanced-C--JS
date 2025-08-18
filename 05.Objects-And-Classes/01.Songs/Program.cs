cd26defad441a7ebd0ff00568cc617f509a9465d
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 19:46:16 +0300

    Songs
    Define a class called Song that will hold the following information about some songs:
    
    · Type List
    
    · Name
    
    · Time
    
    Input / Constraints
    
    · On the first line, you will receive the number of songs - N.
    
    · On the next N lines, you will be receiving data in the following format: "{typeList}_{name}_{time}".
    
    · On the last line, you will receive Type List or "all".
    
    Output
    
    · If you receive Type List as an input on the last line, print out only the names of the songs, which are from that Type List.
    
    · If you receive the "all" command, print out the names of all the songs

---
+++ 05.Objects-And-Classes/01.Songs/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Song_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Song> songsList = new List<Song>();
+
+            int countSongs = int.Parse(Console.ReadLine()); 
+
+            for (int count = 1; count <= countSongs; count++)
+            {
+                string data = Console.ReadLine();
+
+                string typeList = data.Split("_")[0]; 
+                string name = data.Split("_")[1]; 
+                string time = data.Split("_")[2]; 
+
+                Song song = new Song(typeList, name, time);
+
+                songsList.Add(song);
+            }
+
+
+            string typeSongToPrint = Console.ReadLine(); 
+
+            foreach (Song song in songsList)
+            {
+                if (typeSongToPrint == "all" || typeSongToPrint == song.TypeList)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+}
---
+++ 05.Objects-And-Classes/01.Songs/Songs.cs
@@ -0,0 +1,20 @@
+﻿namespace Song_01
+{
+    public class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+        public Song()
+        {
+        }
+
+        public Song(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+        }
+    }
+}
