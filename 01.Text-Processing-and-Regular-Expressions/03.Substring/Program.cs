f5467a9a813a0a5971f61ee0597f8be21bbfc04a
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:54:25 +0300

    Substring
    Write a program that:
    
    · Read first string from the first line of the console
    
    · Reads second string from the second line of the console
    
    · Remove all of the occurrences of the first string in the second string
    
    · Print the remaining string

---
+++ 01.Text-Processing-and-Regular-Expressions/03.Substring/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string partToRemove = Console.ReadLine();
+string text = Console.ReadLine();
+
+while (text.Contains(partToRemove))
+{
+    int startIndex = text.IndexOf(partToRemove);
+    int lenght = partToRemove.Length;
+
+    text.Remove(startIndex, lenght);
+}
