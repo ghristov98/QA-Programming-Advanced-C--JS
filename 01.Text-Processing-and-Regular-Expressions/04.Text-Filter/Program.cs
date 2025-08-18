9bab842e7fd8e61a90a5453e38239d31188d3e7e
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:55:36 +0300

    Text Filter
    Create a program that takes a text and a string of banned words. All words included in the ban list should be replaced with a string of asterisks '*', whose length must be equal to the word's length. The entries in the ban list will be separated by a comma and space ", ". The ban list should be entered on the first input line and the text on the second input line.

---
+++ 01.Text-Processing-and-Regular-Expressions/04.Text-Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string[] bannedWord = Console.ReadLine().Split(" ");
+
+string textToEdit = Console.ReadLine();
+
+foreach (var wordToReplace in bannedWord)
+{
+
+    string asterixWord = "";
+
+    for (int i = 0; i < wordToReplace.Length; i++)
+    {
+        asterixWord += '*';
+    }
+
+    textToEdit = textToEdit.Replace(wordToReplace, asterixWord);
+}
+
+Console.WriteLine(textToEdit);
