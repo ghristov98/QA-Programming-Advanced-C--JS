7c22fc5d008fe915c6fff3bcecf8aa4b7abb9fe4
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 20:03:53 +0300

    Pokemon Trainer
    Define a class Trainer and a class Pokemon.
    
    Trainers have:
    
    · Name
    
    · Number of badges
    
    · A collection of pokemon
    
    Pokemon have:
    
    · Name
    
    · Element
    
    · Health
    
    All values are mandatory. Every Trainer starts with 0 badges.
    
    You will be receiving lines until you receive the command "Tournament". Each line will carry information about a pokemon and the trainer who caught it in the format:
    
    "{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
    
    TrainerName is the name of the Trainer who caught the pokemon. Trainers' names are unique. After receiving the command "Tournament", you will start receiving commands until the "End" command is received. They can contain one of the following:
    
    · "Fire"
    
    · "Water"
    
    · "Electricity"
    
    For every command, you must check if a trainer has at least 1 pokemon with the given element. If he does, he receives 1 badge. Otherwise, all of his pokemon lose 10 health. If a pokemon falls to 0 or less health, he dies and must be deleted from the trainer's collection. In the end, you should print all of the trainers, sorted by the number of badges they have in descending order (if two trainers have the same amount of badges, they should be sorted by order of appearance in the input) in the format:
    
    "{trainerName} {badges} {numberOfPokemon}"

---
+++ 06.Exercise-Objects-and-Classes/04.Pokemon-Trainer/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using _04.PokemonTrainer;
+
+List<Trainer> trainers = new List<Trainer>();
+
+string input = Console.ReadLine();
+
+while (input != "Tournament")
+{
+    string[] data = input.Split(" ");
+
+    string trainerName = data[0];
+    string pokemonName = data[1];
+    string pokemonElement = data[2];
+    int pokemonHealth = int.Parse(data[3]);
+
+    Pokemon newPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+    if (trainers.Any(t => t.Name == trainerName))
+    {
+        Trainer existingTrainer = trainers.FirstOrDefault(t => t.Name == trainerName);
+
+        existingTrainer.Pokemons.Add(newPokemon);
+    }
+    else
+    {
+        Trainer newTrainer = new Trainer(trainerName);
+
+        newTrainer.Pokemons.Add(newPokemon);
+
+        trainers.Add(newTrainer);
+    }
+
+    input = Console.ReadLine();
+}
+
+string element = Console.ReadLine();
+
+while (element != "End")
+{
+    foreach (Trainer currentTrainer in trainers)
+    {
+        if (currentTrainer.Pokemons.Any(p => p.Element == element))
+        {
+            currentTrainer.NumberOfBadges++;
+        }
+        else
+        {
+            foreach (Pokemon pokemon in currentTrainer.Pokemons)
+            {
+                pokemon.Health -= 10;
+            }
+
+            currentTrainer.Pokemons.RemoveAll(p => p.Health <= 0);
+        }
+    }
+
+    element = Console.ReadLine();
+}
+
+foreach (Trainer trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+{
+    Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+}
