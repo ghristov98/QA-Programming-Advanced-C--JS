b5a6d89e19c7998285d1109ae7e14063fe3f74cc
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:35:51 +0300

    Unit Test: Orders
    Test a given method which takes in N number of strings in the form of:
    
    "{product} {price} {quantity}"
    
    It saves each product and quantity and updates the price each time it changes, and finally calculates the total price for each product.

---
+++ 04.Exercise-UnitTesting-Dictionaries-Lambda-and-LINQ/TestApp.Tests/OrdersTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class OrdersTests
+{
+    [Test]
+    public void Test_Order_WithEmptyInput_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = Array.Empty<string>();
+
+        // Act
+        string actual = Orders.Order(input);
+
+        // Assert
+        Assert.That(actual, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 5.97 1",
+            "banana 3.75 1",
+            "orange 1.98 1",
+        };
+
+        string expected = $"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98";
+
+        // Act
+        string actual = Orders.Order(input);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.25 3",
+            "banana 1.50 4",
+            "orange 2.00 1",
+        };
+
+        string expected = $"apple -> 3.75{Environment.NewLine}banana -> 6.00{Environment.NewLine}orange -> 2.00";
+
+        // Act
+        string actual = Orders.Order(input);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.25 4.38",
+            "banana 1.50 8.73",
+            "orange 2.19 1.25",
+        };
+
+        string expected = $"apple -> 5.48{Environment.NewLine}banana -> 13.10{Environment.NewLine}orange -> 2.74";
+
+        // Act
+        string actual = Orders.Order(input);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
