30c11e88ecd46d02144024dc16fcfc4c697ff2bf
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 12:20:59 +0300

    Unit Test String Method: Text Filter
    Test a given method which takes in an array of strings representing banned words and a string representing text and blurs out every banned word found by replacing it with asterisks

---
+++ 02.Exercise-Text-Processing-Regular-Expressions-and-Unit-Testing/TestApp.UnitTests/TextFilterTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+using System;
+using static NUnit.Framework.Constraints.Tolerance;
+
+namespace TestApp.UnitTests;
+
+public class TextFilterTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { "cat", "bear" };
+        string input = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { "quick", "lazy" };
+        string input = "The quick brown fox jumps over the lazy dog";
+        string expected = "The ***** brown fox jumps over the **** dog";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText()
+    {
+        // Arrange
+        string[] emptyArray = Array.Empty<string>();
+        string input = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = TextFilter.Filter(emptyArray, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_Filter_WhenBannedWordsContainWhitespace_ShouldReplaceBannedWord()
+    {
+        // Arrange
+        string[] bannedWords = new string[] { "lazy dog" };
+        string input = "The quick brown fox jumps over the lazy dog";
+        string expected = "The quick brown fox jumps over the ********";
+
+        // Act
+        string result = TextFilter.Filter(bannedWords, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
