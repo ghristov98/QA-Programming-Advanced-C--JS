bd9a0f12dd7f348f713140fd4bb821fd6893beed
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 12:20:12 +0300

    Unit Test String Method: Substring
    Test a given method which takes in a string to be removed and a string as text from which the string to be removed.

---
+++ 02.Exercise-Text-Processing-Regular-Expressions-and-Unit-Testing/TestApp.UnitTests/SubstringTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class SubstringTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromMiddle()
+    {
+        // Arrange
+        string toRemove = "fox";
+        string input = "The quick brown fox jumps over the lazy dog";
+        string expected = "The quick brown jumps over the lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromStart()
+    {
+        // Arrange
+        string toRemove = "Green";
+        string input = "Green fox jumps over the lazy dog";
+        string expected = "fox jumps over the lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesSubstringFromEnd()
+    {
+        // Arrange
+        string toRemove = "dog";
+        string input = "Green fox jumps over the lazy dog";
+        string expected = "Green fox jumps over the lazy";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_RemovesAllOccurrences()
+    {
+        // Arrange
+        string toRemove = "THE";
+        string input = "The quick brown fox jumps over the lazy dog";
+        string expected = "quick brown fox jumps over lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    // tho tests - not for Judge
+
+    [TestCase("cat")]
+    public void Test_RemoveOccurrences_NotExistingToRemove_ReturnSameInput(string toRemove)
+    {
+        // Arrange
+        string input = "The quick brown fox jumps over the lazy dog";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_RemoveOccurrences_EmptyInput_ReturnEmptyString()
+    {
+        // Arrange
+        string toRemove = "fox";
+        string input = "";
+
+        // Act
+        string result = Substring.RemoveOccurrences(toRemove, input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+}
