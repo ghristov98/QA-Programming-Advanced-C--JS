c11cb7d910f50da5820ab622bca61f86f57521d7
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 20:00:55 +0300

    Students
    Create a program that sorts some students by their grade in descending order. Each student should have:
    
    · First name (string)
    
    · Last name (string)
    
    · Grade (a floating-point number)
    
    Input
    
    · On the first line, you will receive a number n - the count of all students.
    
    · On the next n lines, you will be receiving information about these students in the following format: "{first name} {second name} {grade}".
    
    Output
    
    · Print out the information about each student in the following format: "{first name} {second name}: {grade}".

---
+++ 06.Exercise-Objects-and-Classes/01.Students/Program.cs
@@ -0,0 +1,44 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] studentData = Console.ReadLine().Split(" ");
+
+            string firstName = studentData[0];
+            string lastName = studentData[1];
+            double grade = double.Parse(studentData[2]);
+
+            Student currentStudent = new Student(firstName, lastName, grade);
+
+            students.Add(currentStudent);
+        }
+
+        foreach (Student item in students.OrderByDescending(s => s.Grade))
+        {
+            Console.WriteLine($"{item.FirstName} {item.LastName}: {item.Grade:F2}");
+        }
+    }
+}
+
+public class Student
+{
+    public Student(string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public double Grade { get; set; }
+}
