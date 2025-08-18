1fa58c929572fd76b7c0253e8ecdc8d02488c47f
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:57:49 +0300

    Match Phone Number
    Write a program that:
    
    · Reads a text from the console
    
    · Create a regular expression to match a valid phone number from Sofia
    
    · A valid number has the following characteristics:
    
    o It starts with "+359"
    
    o Then, it is followed by the area code (always 2)
    
    o After that, it's followed by the number itself:
    
    o The number consists of 7 digits (separated into two groups of 3 and 4 digits respectively)
    
    o The different parts are separated by either a space or a hyphen ('-')
    
    · Print all valid numbers on the console, separated by a comma and a space ",

---
+++ 01.Text-Processing-and-Regular-Expressions/07.Match-Phone-Number/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Text.RegularExpressions;
+
+string pattern = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+
+string input = Console.ReadLine();
+
+MatchCollection validPhones = Regex.Matches(input, pattern);
+
+Console.WriteLine(string.Join(", ", validPhones));
