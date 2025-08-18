b7047514caf80d350f9641602cc6fb5c4eafc301
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:52:30 +0300

    Repeat Strings
    Write a program that:
    
    · Reads an array of strings from the console
    
    · Each string is repeated N times, where N is the length of the string
    
    · Print the concatenated strin

---
+++ 01.Text-Processing-and-Regular-Expressions/02.Repeat-Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string[] arr = Console.ReadLine().Split(" ");
+
+string result = "";
+
+for (int i = 0; i < arr.Length; i++)
+{
+    string currentText = arr[i];
+
+    string concatText = "";
+
+    for (int count = 0; count < currentText.Length; count++)
+    {
+        concatText += currentText;
+
+    }
+
+    result += concatText;
+}
+Console.WriteLine(result);
