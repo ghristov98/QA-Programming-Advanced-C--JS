27db57cbec402153e4af0fa2054fcb5f35161f61
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:18:26 +0300

    A Miner Task
    You will be given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper and so on) and every even - quantity. Your task is to collect the resources and print them each on a new line.
    
    Print the resources and their quantities in the following format:
    
    "{resource} –> {quantity}"
    
    The quantities will be in the range [1… 2 000000000].

---
+++ 03.Dictionaries-Lambda-and-LINQ/06.Miner-Task/Program.cs
@@ -0,0 +1,26 @@
+﻿Dictionary<string, int> resourcesQuantity = new Dictionary<string, int>();
+
+string resource = Console.ReadLine(); 
+
+while (resource != "stop")
+{
+
+    int quantity = int.Parse(Console.ReadLine()); //количество от полезното изкопаемо
+
+    if (resourcesQuantity.ContainsKey(resource))
+    {
+        resourcesQuantity[resource] += quantity;
+    }
+    else
+    {
+        resourcesQuantity.Add(resource, quantity);
+    }
+
+    resource = Console.ReadLine();
+}
+
+
+foreach (KeyValuePair<string, int> entry in resourcesQuantity)
+{
+    Console.WriteLine(entry.Key + " -> " + entry.Value);
+}
