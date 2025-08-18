a138b6396ff96ffd6f74b10b2b7205d76d621cc5
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:16:08 +0300

    Word Synonyms
    Create a program, which keeps a dictionary with synonyms. The key of the dictionary will be the word. The value will be a list of all the synonyms of that word. You will be given a number n – the count of the words. After each word, you will be given a synonym, so the count of lines you have to read from the console is 2 * n. You will be receiving a word and a synonym each on a separate line like this:
    
    · {word}
    
    · {synonym}
    
    If you get the same word twice, just add the new synonym to the list.
    
    Print the words in the following format:
    
    "{word} - {synonym1, synonym2, …, synonymN}"

---
+++ 03.Dictionaries-Lambda-and-LINQ/03.Word-Synonyms/Program.cs
@@ -0,0 +1,25 @@
+﻿int countWords = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+
+for (int count = 1; count <= countWords; count++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if (!wordSynonyms.ContainsKey(word))
+    {
+        wordSynonyms.Add(word, new List<string>());
+        wordSynonyms[word].Add(synonym);
+    }
+    else
+    {
+        wordSynonyms[word].Add(synonym);
+    }
+}
+
+foreach (KeyValuePair<string, List<string>> pair in wordSynonyms)
+{
+    
+    Console.WriteLine(pair.Key + " - " + string.Join(", ", pair.Value));
+}
