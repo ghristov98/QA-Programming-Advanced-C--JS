6a0b635c1ce0492e5db9b34a17927fe055209eb2
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 19:47:32 +0300

    Students
    Define a class called Student, which will hold the following information about some students:
    
    · first name - string
    
    · last name - string
    
    · age - int
    
    · home town - string
    
    Input
    
    Read information about some students, until you receive the "end" command.
    
    After that, you will receive a city name.
    
    Output
    
    Print the students who are from the given city in the following format:
    
    "{firstName} {lastName} is {age} years old."

---
+++ 05.Objects-And-Classes/02.Students/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string firstName, string lastName, int age, string hometown)
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Hometown = hometown;
+        }
+    }
+}
---
+++ 05.Objects-And-Classes/02.Students/Students.cs
@@ -0,0 +1,40 @@
+﻿namespace Students
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] studentData = input.Split(" ");
+
+                string firstName = studentData[0];      
+                string lastName = studentData[1];       
+                int age = int.Parse(studentData[2]);    
+                string town = studentData[3];           
+
+                Student student = new Student(firstName, lastName, age, town);
+
+                studentsList.Add(student);
+
+
+                input = Console.ReadLine();
+            }
+
+
+            string searchedCity = Console.ReadLine(); 
+
+            foreach (Student student in studentsList)
+            {
+                if (student.Hometown == searchedCity)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+}
