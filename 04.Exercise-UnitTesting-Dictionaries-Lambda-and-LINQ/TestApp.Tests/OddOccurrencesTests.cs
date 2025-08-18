a5a31e8ce6a5d326e772448fe0d905f1b446e56f
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:34:34 +0300

    Unit Test: Odd Occurrence
    Test a given method which takes in an array of strings and finds which words appear an odd number of times.

---
+++ 04.Exercise-UnitTesting-Dictionaries-Lambda-and-LINQ/TestApp.Tests/OddOccurrencesTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.Tests;
+
+public class OddOccurrencesTests
+{
+    [Test]
+    public void Test_FindOdd_WithEmptyArray_ShouldReturnEmptyString()
+    {
+
+        //Arrange
+        string[] input = Array.Empty<string>();
+
+        //Act
+        string result = OddOccurrences.FindOdd(input);
+
+        //Assert
+        Assert.That(result, Is.Empty);
+
+    }
+
+
+    [Test]
+    public void Test_FindOdd_WithNoOddOccurrences_ShouldReturnEmptyString()
+    {
+        //Arrange
+        string[] input = new string[] { "hello", "hello", "softuni", "softuni" };
+
+        //Act
+        string result = OddOccurrences.FindOdd(input);
+
+        //Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_FindOdd_WithSingleOddOccurrence_ShouldReturnTheOddWord()
+    {
+        //Arrange
+        string[] input = new string[] { "Hello", "Hello", "Hello" };
+        string expected = "hello";
+
+        //Act
+        string result = OddOccurrences.FindOdd(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMultipleOddOccurrences_ShouldReturnAllOddWords()
+    {
+        //Arrange
+        string[] input = new string[] { "Hello", "Hello", "Hello", "Desi", "Desi", "Ivan" };
+        string expected = "hello ivan";
+
+        //Act
+        string result = OddOccurrences.FindOdd(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_FindOdd_WithMixedCaseWords_ShouldBeCaseInsensitive()
+    {
+        //Arrange
+        string[] input = new string[] { "HeLLo", "HELLO", "hello", "DESI", "desi", "IVAN" };
+        string expected = "hello ivan";
+
+        //Act
+        string result = OddOccurrences.FindOdd(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
