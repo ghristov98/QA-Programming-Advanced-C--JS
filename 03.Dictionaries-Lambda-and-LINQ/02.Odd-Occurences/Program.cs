72a193116d73080e9d6cd746718845e437fca3d4
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:07:42 +0300

    Odd Occurrences
    Write a program that extracts all elements from a given sequence of words that are present in it an odd number of times (case-insensitive):
    
    · Words are given on a single line, space-separated.
    
    · Print the result elements in lowercase, in their order of appearance.

---
+++ 03.Dictionaries-Lambda-and-LINQ/02.Odd-Occurences/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] words = Console.ReadLine() 
+                  .Split(" ");      
+
+
+
+Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+foreach (string word in words)
+{
+    string wordWithLowerCase = word.ToLower();
+
+    if (wordsCount.ContainsKey(wordWithLowerCase))
+    {
+        wordsCount[wordWithLowerCase]++;
+    }
+    else
+    {
+        wordsCount.Add(wordWithLowerCase, 1);
+    }
+}
+
+
+foreach (string key in wordsCount.Keys)
+{
+    
+    int occurrences = wordsCount[key];
+    if (occurrences % 2 != 0)
+    {
+        Console.Write(key + " ");
+    }
+}
