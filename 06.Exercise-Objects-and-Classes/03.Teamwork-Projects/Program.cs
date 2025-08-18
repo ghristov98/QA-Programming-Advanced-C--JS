00e8f490c857d2d44fee9fe9f803703327d7485f
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 20:02:38 +0300

    Teamwork Projects
    It's time for the teamwork projects and you are responsible for gathering the teams. First, you will receive an integer – the count of the teams you will have to register. You will be given a user and a team, separated with "-". The user is the creator of the team. For every newly created team you should print a message:
    
    "Team {teamName} has been created by {user}!".
    
    Next, you will receive а user with a team, separated with "->", which means that the user wants to join that team. Upon receiving the command: "end of assignment", you should print every team, ordered by the count of its members (descending) and then by name (ascending). For each team, you have to print its members sorted by name (ascending). However, there are several rules:
    
    · If а user tries to create a team more than once, a message should be displayed:
    
    - "Team {teamName} was already created!"
    
    · A creator of a team cannot create another team – the following message should be thrown:
    
    - "{user} cannot create another team!"
    
    · If а user tries to join a non-existent team, a message should be displayed:
    
    - "Team {teamName} does not exist!"
    
    · A member of a team cannot join another team – the following message should be thrown:
    
    - "Member {user} cannot join team {team Name}!"
    
    · In the end, teams with zero members (with only a creator) should disband and you have to print them ordered by name in ascending order.
    
    · Every valid team should be printed ordered by name (ascending) in the following format:

---
+++ 06.Exercise-Objects-and-Classes/03.Teamwork-Projects/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _03.TeamworkProjects
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Team> teams = new List<Team>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split("-");
+                string creator = input[0];
+                string teamName = input[1];
+
+                if (teams.Any(t => t.TeamName == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} was already created!");
+                }
+                else if (teams.Any(t => t.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                }
+                else
+                {
+                    Team currentTeam = new Team(teamName, creator);
+
+                    teams.Add(currentTeam);
+
+                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                }
+            }
+
+            string joinInformation = Console.ReadLine();
+
+            while (joinInformation != "end of assignment")
+            {
+                string[] data = joinInformation.Split("->");
+                string member = data[0];
+                string teamName = data[1];
+
+                if (!teams.Any(t => t.TeamName == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} does not exist!");
+                }
+                else if (teams.Any(t => t.Members.Contains(member)
+                                    || t.Creator == member))
+                {
+                    Console.WriteLine($"Member {member} cannot join team {teamName}!");
+                }
+                else
+                {
+                    Team teamToAdd = teams.FirstOrDefault(t => t.TeamName == teamName);
+
+                    teamToAdd.Members.Add(member);
+                }
+
+                joinInformation = Console.ReadLine();
+            }
+
+            List<Team> validTeams = teams.Where(t => t.Members.Count > 0)
+                                         .OrderByDescending(t => t.Members.Count)
+                                         .ThenBy(t => t.TeamName)
+                                         .ToList();
+
+            foreach (Team team in validTeams)
+            {
+                Console.WriteLine(team.TeamName);
+                Console.WriteLine($"- {team.Creator}");
+
+                foreach (string member in team.Members.OrderBy(m => m))
+                {
+                    Console.WriteLine($"-- {member}");
+                }
+            }
+
+            List<Team> teamsToDisband = teams.Where(t => t.Members.Count == 0)
+                                             .OrderBy(t => t.TeamName)
+                                             .ToList();
+
+            Console.WriteLine("Teams to disband:");
+
+            foreach (Team team in teamsToDisband)
+            {
+                Console.WriteLine(team.TeamName);
+            }
+        }
+    }
+}
---
+++ 06.Exercise-Objects-and-Classes/03.Teamwork-Projects/Team.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _03.TeamworkProjects
+{
+    public class Team
+    {
+        public Team(string teamName, string creator)
+        {
+            TeamName = teamName;
+            Creator = creator;
+            Members = new List<string>();
+        }
+
+        public string TeamName { get; set; }
+
+        public string Creator { get; set; }
+
+        public List<string> Members { get; set; }
+    }
+}
