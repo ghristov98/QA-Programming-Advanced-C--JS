c244dbaecfb0ec4a0e870a55ab27ff3c3acd4840
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 14:17:46 +0300

    Count Real Numbers
    Write a program that:
    
    · Read a list of integers
    
    · Print them in ascending order, along with their number of occurrences in the format:
    
    {number} -> {occurances}

---
+++ 03.Dictionaries-Lambda-and-LINQ/01.Count-Real-Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+SortedDictionary<int, int> countOccurences = new SortedDictionary<int, int>();
+
+foreach (int number in numbers)
+{
+    if (countOccurences.ContainsKey(number))
+    {
+        
+        countOccurences[number]++;
+    }
+    else
+    {
+        countOccurences.Add(number, 1);
+    }
+}
+
+foreach (KeyValuePair<int, int> pair in countOccurences)
+{
+
+    Console.WriteLine(pair.Key + " -> " + pair.Value);
+}
