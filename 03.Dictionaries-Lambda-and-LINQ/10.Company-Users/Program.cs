9c407b77575e5e548a84b7aa103377e688c411d8
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:20:53 +0300

    Company Users
    Create a program that keeps information about companies and their employees.
    
    You will be receiving a company name and an employee's id, until you receive the "End" command. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
    
    When you finish reading the data, print the company's name and each employee's id in the following format:
    
    {companyName}
    
    -- {id1}
    
    -- {id2}
    
    -- {idN}
    
    Input / Constraints
    
    · Until you receive the "End" command, you will be receiving input in the format: "{companyName} -> {employeeId}".
    
    · The input always will be valid.

---
+++ 03.Dictionaries-Lambda-and-LINQ/10.Company-Users/Program.cs
@@ -0,0 +1,37 @@
+﻿Dictionary<string, List<string>> companyEmployees = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+
+while (input != "End")
+{
+    string companyName = input.Split(" -> ")[0]; 
+    string employeeId = input.Split(" -> ")[1]; 
+
+
+    if (companyEmployees.ContainsKey(companyName))
+    {
+        List<string> currentEmployees = companyEmployees[companyName];
+        if (!currentEmployees.Contains(employeeId))
+        {
+            currentEmployees.Add(employeeId);
+        }
+    }
+    else
+    {
+        companyEmployees.Add(companyName, new List<string>() { employeeId });
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (KeyValuePair<string, List<string>> entry in companyEmployees)
+{
+    Console.WriteLine(entry.Key);
+
+    foreach (string employee in entry.Value)
+    {
+        Console.WriteLine("-- " + employee);
+    }
+
+}
