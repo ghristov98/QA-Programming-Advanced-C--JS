45cdd430af6db92118b579ed7121f0ffa4b0b24e
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 20:29:16 +0300

    Square Root
    Write a program that reads an integer number and calculates and prints its square root.
    
    · If the number is negative, print "Invalid number."
    
    · In all cases finally, print "Goodbye."
    
    Use try-catch-finally.

---
+++ 08.Exception-Handling/01.Square-Root/Program.cs
@@ -0,0 +1,21 @@
+﻿
+try
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number < 0)
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+
+    double sqrt = Math.Sqrt(number);
+    Console.WriteLine(sqrt);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
