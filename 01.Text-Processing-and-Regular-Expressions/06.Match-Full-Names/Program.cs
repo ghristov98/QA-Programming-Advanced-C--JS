9e8f587aa6aa6288177c45b0fad1a9f4cec76e4a
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:56:59 +0300

    Match Full Name
    Write a program that:
    
    · Reads a text from the console
    
    · Match full names from the given text
    
    · A valid full name has the following characteristics:
    
    o It consists of two words
    
    o Each word starts with a capital letter
    
    o After the first letter, it only contains lowercase letters afterward
    
    o Each of the two words should be at least two letters long
    
    o The two words are separated by a single space
    
    · Print full names on the console, separated by single space

---
+++ 01.Text-Processing-and-Regular-Expressions/06.Match-Full-Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b[A-Z][a-z]+[A-Z][a-z]+";
+
+string input = Console.ReadLine();
+
+MatchCollection names =  Regex.Matches(input, pattern);
+
+foreach (var currentName in names)
+{
+    Console.Write(currentName + " ");
+}
