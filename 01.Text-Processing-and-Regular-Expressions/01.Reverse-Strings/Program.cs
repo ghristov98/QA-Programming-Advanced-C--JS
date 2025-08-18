a459d6a33f0970f249218f64a2411ff43cb787fc
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:53:13 +0300

    Reverse Strings
    Write a program that:
    
    · Reads a series of strings from the console, until you receive an "end" command
    
    · Reverse given strings
    
    · Print each pair (old text and reversed text) on a separate line in the format:
    
    "{word} = {reversed word}"

---
+++ 01.Text-Processing-and-Regular-Expressions/01.Reverse-Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    // нова променлива за обърнатия вход
+    string reversedInput = "";
+
+    // обикаляме текста от входа отзад напред
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        // добавяме всеки символ отзад напред
+        reversedInput += input[i];
+    }
+
+    // печатаме изхода
+    Console.WriteLine($"{input} = {reversedInput}");
+
+    // прочитаме нов вход от конзолата
+    input = Console.ReadLine();
+}
