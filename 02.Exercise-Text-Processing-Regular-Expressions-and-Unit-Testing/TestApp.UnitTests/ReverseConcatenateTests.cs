3405b99015dd5b422d6ff192f71fcb5826b23aeb
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 12:19:28 +0300

    Unit Test String Method: Reverse and Concatenate
    Test a given method which takes in an array of strings puts the words in a reverse order and concatenates them together.

---
+++ 02.Exercise-Text-Processing-Regular-Expressions-and-Unit-Testing/TestApp.UnitTests/ReverseConcatenateTests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.UnitTests;
+
+public class ReverseConcatenateTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_EmptyInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string[] emptyArray = Array.Empty<string>();
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_SingleString_ReturnsSameString()
+    {
+        // Arrange
+        string[] input = new string[] { "fox" };
+        string expected = "fox";
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_MultipleStrings_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[] input = new string[] { "fox", "lazy", "mad", "dog" };
+        string expected = "dogmadlazyfox";
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_NullInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string[] nullArray = null;
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(nullArray);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_WhitespaceInput_ReturnsConcatenatedString()
+    {
+        // Arrange
+        string[] input = new string[] { "fox", " ", " crazy", "mad" };
+        string expected = "mad crazy fox";
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_LargeInput_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[] input = new string[1000];
+
+        for (int i = 0; i < 1000; i++)
+        {
+            input[i] = i.ToString();
+        }
+
+        StringBuilder expected = new StringBuilder();
+
+        for (int i = 999; i >= 0; i--)
+        {
+            expected.Append(i);
+        }
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected.ToString()));
+    }
+}
