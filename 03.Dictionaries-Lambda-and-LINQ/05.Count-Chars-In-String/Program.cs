48b72076035e50282e63d6537d1b3363ebd4df33
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:17:52 +0300

    Count Chars in a String
    Create a program that counts all characters in a string, except for space (' ').
    
    Print all the occurrences in the following format:
    
    "{char} -> {occurrences}"

---
+++ 03.Dictionaries-Lambda-and-LINQ/05.Count-Chars-In-String/Program.cs
@@ -0,0 +1,26 @@
+﻿string text = Console.ReadLine();
+
+Dictionary<char, int> countSymbols = new Dictionary<char, int>();
+
+
+foreach (char symbol in text)
+{
+    if (symbol == ' ')
+    {
+        continue;
+    }
+
+    if (countSymbols.ContainsKey(symbol))
+    {
+       countSymbols[symbol]++;
+    }
+    else
+    {
+        countSymbols.Add(symbol, 1);
+    }
+}
+
+foreach (KeyValuePair<char, int> pair in countSymbols)
+{
+    Console.WriteLine(pair.Key + " -> " + pair.Value);
+}
