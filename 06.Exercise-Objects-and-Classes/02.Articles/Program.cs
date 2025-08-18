126b966b632341bd54bd0af63d7b2647dd323d9a
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 20:01:35 +0300

    Articles
    Create a class Article with the following properties:
    
    · Title – a string
    
    · Content – a string
    
    · Author – a string
    
    The class should have a constructor and the following methods:
    
    · Edit (new content) – change the old content with the new one
    
    · ChangeAuthor (new author) – change the author
    
    · Rename (new title) – change the title of the article
    
    · Override the ToString method – print the article in the following format:
    
    "{title} - {content}: {author}"
    
    Create a program that reads an article in the following format "{title}, {content}, {author}". On the next line, you will receive a number n, representing the number of commands, which will follow after it. On the next n lines, you will be receiving the following commands:
    
    · "Edit: {new content}"
    
    · "ChangeAuthor: {new author}"
    
    · "Rename: {new title}"
    
    In the end, print the final state of the article.

---
+++ 06.Exercise-Objects-and-Classes/02.Articles/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] articleData = Console.ReadLine().Split(", ");
+        string title = articleData[0];
+        string content = articleData[1];
+        string author = articleData[2];
+
+        Article article = new Article(title, content, author);
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] cmdArg = Console.ReadLine().Split(": ");
+            string command = cmdArg[0];
+            string newData = cmdArg[1];
+
+            if (command == "Edit")
+            {
+                article.Edit(newData);
+            }
+            else if (command == "ChangeAuthor")
+            {
+                article.ChangeAuthor(newData);
+            }
+            else if (command == "Rename")
+            {
+                article.Rename(newData);
+            }
+        }
+
+        Console.WriteLine(article.ToString());
+    }
+}
+
+public class Article
+{
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public string Title { get; private set; }
+
+    public string Content { get; private set; }
+
+    public string Author { get; private set; }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
