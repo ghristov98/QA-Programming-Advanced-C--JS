5fcbc39abc2f2b3390ca799209f469aa510d2a52
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 15:35:14 +0300

    Unit Test: Miner
    Test a given method which takes in N number of strings in the form of:
    
    "{mineral} {quantity}"
    
    Then it counts the total quantity of a given mineral and returns a string showing that.

---
+++ 04.Exercise-UnitTesting-Dictionaries-Lambda-and-LINQ/TestApp.Tests/MinerTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.Tests;
+
+public class MinerTests
+{
+    [Test]
+    public void Test_Mine_WithEmptyInput_ShouldReturnEmptyString()
+    {
+        //Arrange
+        string[] input = Array.Empty<string>();
+
+        //Act
+        string result = Miner.Mine(input);
+
+        //Assert
+        Assert.That(result, Is.Empty);
+    }
+
+
+    [Test]
+    public void Test_Mine_WithMixedCaseResources_ShouldBeCaseInsensitive()
+    {
+        //Arrange
+        string[] input = new string[] { "GOLD 2", "silver 5", "Gold 10", "SILVER 23", "Copper 12" };
+        string expected = "gold -> 12"
+                            + Environment.NewLine
+                            + "silver -> 28"
+                            + Environment.NewLine
+                            + "copper -> 12";
+
+        //Act
+        string result = Miner.Mine(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Mine_WithDifferentResources_ShouldReturnResourceCounts()
+    {
+        //Arrange
+        string[] input = new string[] { "copper 10", "gold 20", "silver 5", "gold 10", "silver 25", "copper 30" };
+        string expected = "copper -> 40"
+                            + Environment.NewLine
+                            + "gold -> 30"
+                            + Environment.NewLine
+                            + "silver -> 30";
+
+        //Act
+        string result = Miner.Mine(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
