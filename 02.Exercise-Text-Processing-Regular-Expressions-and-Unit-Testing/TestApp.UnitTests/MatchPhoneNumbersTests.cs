05f554f6b9fcd64057b173429e491c638d3dd2eb
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 12:14:58 +0300

    Unit Test Regular Expression: Match Phone Numbers
    Test a given method which takes in a string of phone numbers and matches phones in the form of +359 followed by either a space or a hyphen, then the area code '2,' followed by three digits, and finally, four more digits at the end.

---
+++ 02.Exercise-Text-Processing-Regular-Expressions-and-Unit-Testing/TestApp.UnitTests/MatchPhoneNumbersTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class MatchPhoneNumbersTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_Match_ValidPhoneNumbers_ReturnsMatchedNumbers()
+    {
+        // Arrange
+        string phoneNumbers = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555";
+        string expected = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555";
+
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_NoValidPhoneNumbers_ReturnsEmptyString()
+    {
+        string phoneNumbers = "+359-5-124-5678, +359-2 986 5432, +359-2-5555-5555, +359-2-555-555 ";
+
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Match_EmptyInput_ReturnsEmptyString()
+    {
+        string phoneNumbers = "";
+
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Match_MixedValidAndInvalidNumbers_ReturnsOnlyValidNumbers()
+    {
+        // Arrange
+        string phoneNumbers = "+359-2-124-5678, +359 2-986 5432, +359-2-555-5555, +559-2-555-5555, +359-2-555-555, +359-2-5556-5555";
+        string expected = "+359-2-124-5678, +359-2-555-5555";
+
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
