2a6f6925f31cfe633f59756f8beba0bdbe6a13cb
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 11:56:18 +0300

    Digits, Letters and Others
    Write a program that:
    
    · Read a single string from the console
    
    · Print all the digits on the first line
    
    · Print all the letters on the second line
    
    · Print all the other characters on the third line
    
    Note: There will always be at least one digit, one letter and one other character.

---
+++ 01.Text-Processing-and-Regular-Expressions/05.Digits-Letters-And-Others/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text;
+
+string input = Console.ReadLine();
+
+string letters = "";
+string digits = "";
+string specialCharacters = "";
+
+foreach (char currentSymbol in input)
+{
+    if (char.IsLetter(currentSymbol))
+    {
+        letters += currentSymbol;
+    }
+    else if (char.IsDigit(currentSymbol))
+    {
+        digits += currentSymbol;
+    }
+    else
+    {
+        specialCharacters += currentSymbol;
+    }
+}
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(specialCharacters);
