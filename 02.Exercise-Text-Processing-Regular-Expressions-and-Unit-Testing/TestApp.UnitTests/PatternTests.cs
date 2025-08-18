8ff807341b8a84619eb40f2d72bd024a21ea8c4e
Georgi Hristov <ghristov5208@gmail.com>
2025-08-18 12:17:36 +0300

    Unit Test String Method: Pattern
    Test a given method which takes in a string and a number indicating repetition count then turning every even letter to lowercase and every odd letter to uppercase and repeats this process as much times as specified.

---
+++ 02.Exercise-Text-Processing-Regular-Expressions-and-Unit-Testing/TestApp.UnitTests/PatternTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    // TODO: finish the test cases
+    [TestCase("hello", 2, "hElLohElLo")]
+    [TestCase("abcd", 4, "aBcDaBcDaBcDaBcD")]
+    [TestCase("-go ", 3, "-Go -Go -Go ")]
+    public void Test_GeneratePatternedString_ValidInput_ReturnsExpectedResult(string input,
+        int repetitionFactor, string expected)
+    {
+        // Arrange
+        // Act
+        string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_EmptyInput_ThrowsArgumentException()
+    {
+        // Arrange
+        string emptyInput = "";
+        int repetitionFactor = 3;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Pattern.GeneratePatternedString(emptyInput, repetitionFactor));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_NegativeRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange
+        int repetitionFactor = -5;
+        string input = "hello";
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Pattern.GeneratePatternedString(input, repetitionFactor));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_ZeroRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange
+        int repetitionFactor = 0;
+        string input = "hello";
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Pattern.GeneratePatternedString(input, repetitionFactor));
+    }
+}
