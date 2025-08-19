8b511e4a7531a8d242eb8f0a6da66a232df6cf8b
Georgi Hristov <ghristov5208@gmail.com>
2025-08-19 16:27:12 +0300

    Workforce Management
    The object should have the following functionality:
    
    recruitStaff (name, role, experience) - A function that accepts three parameters: string, string, and number.
    
    o If the value of the string role is different from "Developer", throw an error: `We are not currently hiring for this role.`
    
    o To be hired, the employee must meet the following requirements:
    
    § If the experience is greater than or equal to 4, return the string:
    
    `{name} has been successfully recruited for the role of {role}.`
    
    o Otherwise, if the above conditions are not met, return the following message:
    
    `{name} is not suitable for this role.`
    
    o There is no need for validation for the input, you will always be given a string, string, and number.
    
    · computeWages (hoursWorked) - A function that accepts one parameter: number.
    
    o Workers in this company receive equal pay per hour and this is BGN 18.
    
    o You need to calculate the salary by multiplying the pay for one hour by the number of hours.
    
    o Also, if the employee has been working for more than 160 hours, he must receive an additional BGN 1500 bonus.
    
    o Finally, return the employee's salary.
    
    o You need to validate the input, if the hoursWorked are not a number, or are a negative number, throw an error: "Invalid hours".
    
    · dismissEmployee (workforce, employeeIndex) - A function that accepts an array and number.
    
    o The workforce array will store the names of its employees (["Petar", "Ivan", "George"…]).
    
    o You must remove an element (employee) from the array that is located on the employeeIndex specified as a parameter.
    
    o Finally, return the changed array of workforce as a string, joined by a comma and a space.
    
    o There is a need for validation for the input, an array and index may not always be valid. In case of submitted invalid parameters, throw an error "Invalid input":
    
    § If passed workforce parameter is not an array.
    
    § If the employeeIndex is not a number and is outside the limits of the array.

---
+++ 16.Exercise-UnitTesting-JavaScript/06.Workforce-Manangement-Resources/test/workforceManagement.test.js
@@ -0,0 +1,49 @@
+import { workforceManagement } from '../workforceManagement.js'
+import { expect } from 'chai'
+
+describe('Test_workforceManagement', () => {
+
+    describe('recruitStaff', () => { //(name - string, role - string, experience - number)
+        it('should return correct message', () => {
+            expect(workforceManagement.recruitStaff('Ivanov', 'Developer', 0))
+            .to.equal('Ivanov is not suitable for this role.');
+
+            expect(workforceManagement.recruitStaff('Petrov', 'Developer', 4))
+            .to.equal('Petrov has been successfully recruited for the role of Developer.');
+
+        });
+        
+        it('shoud throw an error', () => {
+            expect(() => workforceManagement.recruitStaff('Ivanov', 'QA', 0))
+            .to.throw('We are not currently hiring for this role.');
+        });
+    });
+
+    describe('computeWages', () => { // hoursWorked - number
+        it('should throw error incorrect hoursWorked', () => {
+            expect(() => workforceManagement.computeWages('asd')).to.throw('Invalid hours');
+            expect(() => workforceManagement.computeWages(-5)).to.throw('Invalid hours');            
+        });
+
+        it('should return correct payment', () => {
+            expect(workforceManagement.computeWages(100)).to.equal(1800);
+            expect(workforceManagement.computeWages(200)).to.equal(5100);
+        });
+    });
+
+    describe('dismissEmployee', () => { //workforce - [string], employeeIndex - int index
+        it('should throw error invalid input', () => {
+            expect(() => workforceManagement.dismissEmployee()).to.throw('Invalid input');
+            expect(() => workforceManagement.dismissEmployee(['Ivan', 'Dragan'], 3.14)).to.throw('Invalid input');
+            expect(() => workforceManagement.dismissEmployee(['Ivan', 'Dragan'], 3)).to.throw('Invalid input');
+            expect(() => workforceManagement.dismissEmployee(['Ivan', 'Dragan'], -1)).to.throw('Invalid input');
+            expect(() => workforceManagement.dismissEmployee({}, 1)).to.throw('Invalid input');
+            expect(() => workforceManagement.dismissEmployee(undefined, 1)).to.throw('Invalid input');
+        });
+
+        it('should return correct message', () => {
+            expect(workforceManagement.dismissEmployee(['Ivan', 'Dragan'], 1)).to.equal('Ivan');
+            expect(workforceManagement.dismissEmployee(['Ivan', 'Dragan', 'Petkan'], 1)).to.equal('Ivan, Petkan');
+        });
+    });
+});
