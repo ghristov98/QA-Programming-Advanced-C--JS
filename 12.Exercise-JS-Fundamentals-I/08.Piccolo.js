80a1b81bfe39899037a9635709a1cd71ccb769f4
Georgi Hristov <ghristov5208@gmail.com>
2025-08-19 15:59:45 +0300

    Piccolo
    Write a function that:
    
    · Records a car number for every car that enters the parking lot
    
    · Removes a car number when the car goes out
    
    · Input will be an array of strings in format [direction, carNumber]
    
    Print the output with all car numbers which are in the parking lot sorted in ascending by number.
    
    If the parking lot is empty, print: "Parking Lot is Empty".

--- 12.Exercise-JS-Fundamentals-I/08.Piccolo.js
+++ 12.Exercise-JS-Fundamentals-I/08.Piccolo.js
@@ -1,1 +1,42 @@
+function piccolo (array) {
 
+    let parking = []; 
+
+    for (let command of array) {
+        let action = command.split(', ')[0]; 
+        let carNumber = command.split(', ')[1];
+
+        if (action === 'IN') {
+            if (!parking.includes(carNumber)){
+                parking.push(carNumber);
+            }
+        } else if (action === 'OUT') {
+            let position = parking.indexOf(carNumber);
+            if (position !== -1) {
+                parking.splice(position, 1);
+            }
+        }
+    }
+
+    parking.sort(); 
+
+    if (parking.length === 0) {
+        console.log('Parking Lot is Empty')
+    } else {
+        for (let carNumber of parking) {
+            console.log(carNumber);
+        }
+    }
+}
+
+piccolo(['IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'IN, CA9999TT',
+'IN, CA2866HI',
+'OUT, CA1234TA',
+'IN, CA2844AA',
+'OUT, CA2866HI',
+'IN, CA9876HH',
+'IN, CA2822UU']
+);
