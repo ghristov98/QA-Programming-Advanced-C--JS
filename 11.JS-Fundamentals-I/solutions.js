25f8b641535f37992b2318e129b720d011d0e997
Georgi Hristov <ghristov5208@gmail.com>
2025-08-19 15:53:02 +0300

    Add files via upload
    1. Sum First and Last Array Elements
    
    Write a function that receives an array of numbers and prints the sum of the first and last element in that array
    
    2. Day of Week
    
    Write a program, which receives a number and prints the corresponding name of the day of the week (in English).
    
    If the number is NOT a valid day, print: "Invalid day!".
    
    3. Reverse an Array of Numbers
    
    Write a program, which receives a number n and an array of elements. Your task is to create a new array with n numbers from the original array, reverse it and print its elements on a single line, space-separated.
    
    4. Reverse In Place
    
    Write a program, which receives an array of strings. Your task is to reverse the array without creating a new array. Print the resulting elements on a single line, space-separated. 
    
    5. Phone Book
    
    Write a function that stores information about a person's name and phone number. The input is an array of strings with space-separated name and number. Replace duplicate names. Print the result as shown.
    
    6. Meetings
    
    Write a function that manages meeting appointments. The input comes as an array of strings. Each string contains a weekday and person's name. For each successful meeting, print a message. If you receive the same weekday twice, the meeting cannot be scheduled so print a conflicting message. In the end, print a list of all successful meetings
    
    7. Address Book
    
    Write a function that stores information about a person's name and their address. The input comes as an array of strings. Each string contains the name and the address separated by a colon. If you receive the same name twice just replace the address. In the end, print the full list, sorted alphabetically by the person’s name

--- 11.JS-Fundamentals-I/solutions.js
+++ 11.JS-Fundamentals-I/solutions.js
@@ -1,1 +1,109 @@
+//01. Sum First and Last Array Elements
+
+function solve(arr){
+    let firstElement = Number(arr[0])
+    let lastElement = Number(arr[arr.length-1])
+
+    console.log(firstElement + lastElement)
+}
+
+
+//02. Day of Week
+
+function dayOfWeek(num){
+    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    if(num >= 1 && num <= 7){
+        console.log(daysOfWeek[num-1])
+    } else {
+        console.log("Error")
+    }
+}
+
+
+//03. Reverse an Array of Numbers
+
+function reverseArr(n, arr){
+    let newArr = arr.slice(0, n).reverse()
+
+    console.log(newArr.join(' '))
+}
+
+
+//04. Reverse in Place
+function reverseInPlace(arr){
+    for (let i = 0; i < arr.length / 2; i++) {
+        let currentElement = arr[i] 
+
+        arr[i] = arr[arr.length - 1 - i] 
+        
+        arr[arr.length - 1 - i] = currentElement 
+    }
+
+    console.log(arr.join(' '))
+}
+
+
+// 05. Phone Book
+
+function phoneBook(input){
+    let phoneBook = {}
+
+    for (const element of input) {
+        let currentElement = element.split(' ')
+
+        let key = currentElement[0]   
+        let value = currentElement[1] 
+
+        phoneBook[key] = value
+    }
+
+    for (const key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`)
+    }
+}
+
+
+
+// 06. Meetings
+
+function meetings(inputArr){
+
+    let meetings = {}
+
+    for (const element of inputArr) {
+        let [weekday, name] = element.split(' ')
+
+        if(meetings.hasOwnProperty(weekday)){
+            console.log(`Conflict on ${weekday}!`)
+        } else {
+            meetings[weekday] = name
+
+            console.log(`Scheduled for ${weekday}`)
+        }
+    }
+
+    for (const key in meetings) {
+        console.log(`${key} -> ${meetings[key]}`)
+    }
+}
+
+
+// 07. Address Book
+
+function addressBook(inpurArr){
+    let addressBook = {}
+
+    for (const element of inpurArr) {
+        let [name, address] = element.split(':')
+
+        addressBook[name] = address 
+    }
+
+    let sortedAddressBook = Object.entries(addressBook).sort((a,b) => a[0].localeCompare(b[0]))
+
+    for (const [key, value] of sortedAddressBook) {
+        console.log(`${key} -> ${value}`)
+    }
+}
 
