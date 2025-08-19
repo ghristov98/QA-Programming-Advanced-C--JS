171e3cd21d0cc96a5c664081223ce0def52cf054
Georgi Hristov <ghristov5208@gmail.com>
2025-08-19 15:50:13 +0300

    Add files via upload
    1. Multiply the Number by 2
    
    Write a function that receives a number and prints as result that number multiplied by two.
    
    2. Student Information
    
    You will be given 3 parameters – student name (string), age (number) and average grade (number). Your task is to print all the info about the student in the following format:
    
    "Name: {student name}, Age: {student age}, Grade: {student grade}"
    
    Note: The grade should be formatted to the second decimal point
    
    3. Excellent Grade
    
    Write a function that receives a single number and checks if the grade is excellent or not. If it is, print "Excellent", otherwise print "Not excellent".
    
    4. Month Printer
    
    Write a program, that takes an integer as a parameter and prints the corresponding month. If the number is more than 12 or less than 1, print "Error!"
    
    Input
    
    You will receive a single number.
    
    Output
    
    If the number is within the boundaries, print the corresponding month, otherwise print "Error!"
    
    4. Month Printer
    
    Write a program, that takes an integer as a parameter and prints the corresponding month. If the number is more than 12 or less than 1, print "Error!"
    
    Input
    
    You will receive a single number.
    
    Output
    
    If the number is within the boundaries, print the corresponding month, otherwise print "Error!"
    
    6. Largest Number
    
    Write a function that takes three number arguments as input and finds the largest of them.
    
    Print the following text on the console: "The largest number is {number}.".
    
    The input comes as three number arguments passed to your function.
    
    The output should be printed to the console. 
    
    7. Theatre Promotions
    
    A theatre is doing a ticket sale, but they need a program to calculate the price of a single ticket. If the given age does not fit one of the categories, you should print "Error!". You can see the prices in the table below: Input
    
    The input comes in two parameters. The first one will be the type of day (string). The second is the age of the person (number).
    
    Output
    
    Print the price of the ticket according to the table, or "Error!" if the age is not in the table.
    
    Constraints
    
    · The age will be in the interval [-1000…1000].
    
    · The type of day will always be valid. 
    
    8. Circle Area
    
    Write a function that takes a single argument as input. Check the type of input argument. If it is a number, assume it is the radius of a circle and calculate the circle area. Print the area rounded to two decimal places.
    
    If the argument type is NOT a number, print the following text on the console: "We can not calculate the circle area, because we received a {type of argument}."
    
    The input comes as a single argument passed to your function.
    
    The output should be printed on the console.
    
    9. Numbers from 1 to 5
    
    Write a function that prints all the numbers from 1 to 5 (inclusive) each on a separate line.
    
    Hints
    
    Create a for loop starting from 1 and continuing until 5 and print the number
    
    10. Numbers from M to N
    
    Write a function that receives a number M and a number N (M will always be bigger than N). Print all numbers from M to N.

--- 10.Javascript-Basics/solutions.js
+++ 10.Javascript-Basics/solutions.js
@@ -1,1 +1,221 @@
+'use strict'
+//01. Multiply the Number by 2
 
+function solve(num){
+    return num * 2
+}
+
+console.log(solve(5))
+
+//02. Student Information
+
+function solve(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+solve('Pesho', 22, 5.9)
+
+//03. Excellent Grade
+
+function solve(grade){
+    if (grade >= 5.50){
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+
+solve(5.9)
+
+//04. Month Printer
+
+function solve(num){
+
+    let result;
+
+    switch(num){
+        case 1:
+            result = 'January'
+            break
+        case 2:
+            result = 'February'
+            break
+        case 3:
+            result = 'March'
+            break
+        case 4:
+            result = 'April'
+            break
+        case 5:
+            result = 'May'
+            break
+        case 6:
+            result = 'June'
+            break
+        case 7:
+            result = 'July'
+            break
+        case 8:
+            result = 'August'
+            break
+        case 9:
+            result = 'September'
+            break
+        case 10:
+            result = 'October'
+            break
+        case 11:
+            result = 'November'
+            break
+        case 12:
+            result = 'December'
+            break
+        default:
+            result = 'Error!'
+            break
+    }
+
+    console.log(result)
+}
+
+solve(12)
+
+//05. Math Operations
+
+function solve(a, b, operator){
+    let result;
+
+    if (operator === '+'){
+        result = a + b;
+    } else if (operator === '-'){
+        result = a - b;
+    } else if (operator === '*'){
+        result = a * b;
+    } else if (operator === '/'){
+        result = a / b;
+    } else if (operator === '%'){
+        result = a % b;
+    } else if (operator === '**'){
+        result = a ** b;
+    }
+
+    console.log(result)
+}
+
+solve(5, 5, '+')
+solve(5, 5, '-')
+solve(5, 5, '*')
+solve(5, 5, '/')
+solve(7, 5, '%')
+solve(5, 3, '**')
+
+//06. Largest Number
+
+function solve(a, b, c){
+    let maxNumber = Number.MIN_SAFE_INTEGER;
+
+    if(a > maxNumber){
+        maxNumber = a;
+    }
+
+    if(b > maxNumber){
+        maxNumber = b;
+    }
+
+    if(c > maxNumber){
+        maxNumber = c;
+    }
+
+    console.log(`The largest number is ${maxNumber}.`)
+}
+
+solve(3, -5, 12)
+solve(13, 5, 12)
+solve(-3, -5, -12)
+
+
+//07. Theatre Promotions
+
+function solve(tariff, clientAge) {
+    
+    let price = 0;
+
+    if (0 <= clientAge && clientAge <= 18) {
+        switch(tariff) {
+            case "Weekday": 
+                price = 12;
+                break;
+                case "Weekend":
+                price = 15;
+                break;
+            case "Holiday":
+                price = 5;
+                break;
+        }
+    } else if (18 < clientAge && clientAge <=64) {
+        switch(tariff) {
+            case "Weekday": 
+                price = 18;
+                break;
+            case "Weekend":
+                price = 20;
+                break;
+            case "Holiday":
+                price = 12;
+                break;
+        }
+    } else if (64 < clientAge && clientAge <=122) {
+        switch(tariff) {
+            case "Weekday": 
+                price = 12;
+                break;
+            case "Weekend":
+                price = 15;
+                break;
+            case "Holiday":
+                price = 10;
+                break;
+            }
+    } else {
+        console.log("Error!");
+    }
+
+    if (price > 0) {
+        console.log(`${price}$`);
+    }
+}
+
+//08. Circle Area
+
+function solve(input){
+
+    if(typeof input === 'number'){
+        let area = Math.pow(input, 2) * Math.PI;
+
+        console.log(area.toFixed(2))
+    } else {
+        console.log(`We can not calculate the circle area, because we received a ${typeof input}.`)
+    }
+
+}
+
+solve(5)
+solve('pet')
+
+//09. Numbers from 1 to 5
+
+function solve()
+{
+    for (i = 1 ; i <=5 ; i++)
+    {
+        console.log(i);
+    }
+}
+
+//10. Numbers from M to N
+
+function solve(m, n) {
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
