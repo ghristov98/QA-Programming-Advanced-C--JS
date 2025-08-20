d45e62ec235e4d1c210f9065e99fdb9225faae64
Georgi Hristov <ghristov5208@gmail.com>
2025-08-20 08:29:12 +0300

    Update chooseYourCar.test.js

--- 18.Exam-Prep-II/03.Choose-Your-Car/test/chooseYourCar.test.js
+++ 18.Exam-Prep-II/03.Choose-Your-Car/test/chooseYourCar.test.js
@@ -5,12 +5,9 @@ import { expect, assert } from 'chai';
 describe('Test chooseYourCar', () => {
     describe('choosingType', () => {
         it('should throw an error on invalid input', () => {
-            //invalid input -> невалидна година и невалиден вид кола
-            //невалидна година: година < 1900 или година > 2022
             expect(() => chooseYourCar.choosingType("Sedan", "red", 1800)).to.throw(Error, 'Invalid Year!');
             expect(() => chooseYourCar.choosingType("Sedan", "black", 2024)).to.throw(Error, 'Invalid Year!');
 
-            //невалиден вид кола: вид различен от Седан
             expect(() => chooseYourCar.choosingType("Truck", "blue", 2007)).to.throw(Error, 'This type of car is not what you are looking for.');
         });
         it('should meet requirments for a car', () => {
@@ -44,15 +41,10 @@ describe('Test chooseYourCar', () => {
     describe('brandName', () => {
         it('should throw an error on invalid input', () => {
             //invalid input
-            //1. brands да не е масив
             expect(() => chooseYourCar.brandName("Audi", 3)).to.throw(Error, "Invalid Information!");
-            //2. brandIndex не е цяло число
              expect(() => chooseYourCar.brandName(["Audi", "BMW"], 3.5)).to.throw(Error, "Invalid Information!");
-            //3. brandIndex е цяло число < 0
             expect(() => chooseYourCar.brandName(["Audi", "BMW"], -1)).to.throw(Error, "Invalid Information!");
-            //4. brandIndex е цяло число >= дължина на масив
              expect(() => chooseYourCar.brandName(["Audi", "BMW"], 5)).to.throw(Error, "Invalid Information!");
-             //5. brands да не е масив и brandIndex не е валиден
              expect(() => chooseYourCar.brandName(123, 0)).to.throw(Error, "Invalid Information!");
         });
 
@@ -73,13 +65,9 @@ describe('Test chooseYourCar', () => {
     describe('carFuelConsumption', () => {
         it('should throw an error on invalid input', () => {
             //invalid input
-            //1. разстоянието да не е число
             expect(() => chooseYourCar.carFuelConsumption("distance", 12)).to.throw(Error, "Invalid Information!");
-            //2. разстоянието е число <= 0
             expect(() => chooseYourCar.carFuelConsumption(-10, 12)).to.throw(Error, "Invalid Information!");
-            //3. литрите не са число
             expect(() => chooseYourCar.carFuelConsumption(100, "liters")).to.throw(Error, "Invalid Information!");
-            //4. литрите са число <= 0
             expect(() => chooseYourCar.carFuelConsumption(100, -10)).to.throw(Error, "Invalid Information!");
         });
 
@@ -109,4 +97,5 @@ describe('Test chooseYourCar', () => {
             expect(result).to.equal(expected);
         });
     });
+
 });
